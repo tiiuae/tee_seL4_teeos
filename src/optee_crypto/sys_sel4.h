8863b6db44cceaefa09915da7d88978c2673fd4c
Jouni Ukkonen <jouni.ukkonen@unikie.com>
2022-03-29 13:31:54 +0300

    Remove unneeded function prototypes
    Signed-off-by: Jouni Ukkonen <jouni.ukkonen@unikie.com>
    Change-Id: I42ebf2e1cffc9f7da101e87da324db0d125d0559

--- src/optee_crypto/sys_sel4.h
+++ src/optee_crypto/sys_sel4.h
@@ -38,28 +38,10 @@ int sys_reseed_fortuna_rng(void);
 		trace_printf_helper(TRACE_ERROR, true, __VA_ARGS__); \
 	} while (0)
 
-TEE_Result sys_map_zi(size_t num_bytes, uint32_t flags, vaddr_t *va,
-		      size_t pad_begin, size_t pad_end);
-TEE_Result sys_unmap(vaddr_t va, size_t num_bytes);
-TEE_Result sys_open_ta_bin(const TEE_UUID *uuid, uint32_t *handle);
-TEE_Result sys_close_ta_bin(uint32_t handle);
-TEE_Result sys_map_ta_bin(vaddr_t *va, size_t num_bytes, uint32_t flags,
-			  uint32_t handle, size_t offs, size_t pad_begin,
-			  size_t pad_end);
-TEE_Result sys_copy_from_ta_bin(void *dst, size_t num_bytes, uint32_t handle,
-				size_t offs);
-TEE_Result sys_set_prot(vaddr_t va, size_t num_bytes, uint32_t flags);
-TEE_Result sys_remap(vaddr_t old_va, vaddr_t *new_va, size_t num_bytes,
-		     size_t pad_begin, size_t pad_end);
-TEE_Result sys_gen_random_num(void *buf, size_t blen);
-
 void init_sel4_mempool(void);
 
 TEE_Result entry_open_session_sel4(unsigned long session_id,
 			struct utee_params *up);
-
-TEE_Result entry_invoke_command2(unsigned long session_id,
-			struct utee_params *up, unsigned long cmd_id);
 
 TEE_Result entry_invoke_command_sel4(unsigned long session_id,
 			TEE_Param params[TEE_NUM_PARAMS], uint32_t param_types, unsigned long cmd_id);
