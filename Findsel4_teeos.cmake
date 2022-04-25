413e87e8a716300fa63efb1d2b5f65c6f453b9f3
Tero Salminen <tero.salminen@unikie.com>
2022-04-25 11:28:18 +0300

    Add cmake find module support for sel4_teeos
    Signed-off-by: Tero Salminen <tero.salminen@unikie.com>
    Change-Id: I101c97b0430eaded4fc7a051e48d70c416af8f45

---
+++ Findsel4_teeos.cmake
@@ -0,0 +1,21 @@
+#
+# Copyright 2022, Unikie
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+set(SEL4_TEEOS_DIR "${CMAKE_CURRENT_LIST_DIR}" CACHE STRING "")
+set(SEL4_TEEOS_INCLUDE "${CMAKE_CURRENT_LIST_DIR}/include" CACHE STRING "")
+
+mark_as_advanced(SEL4_TEEOS_DIR SEL4_TEEOS_PATH)
+
+function(sel4_teeos_import_project)
+    include("${SEL4_TEEOS_DIR}/sel4_teeos.cmake")
+endfunction()
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(sel4_teeos
+    DEFAULT_MSG
+    SEL4_TEEOS_DIR
+    SEL4_TEEOS_INCLUDE
+)
---
+++ sel4_teeos.cmake
@@ -0,0 +1,16 @@
+#
+# Copyright 2021, Unikie
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+project(sel4_teeos C CXX ASM)
+
+set(configure_string "")
+
+# rdtime-counter frequency
+config_set(HwTimerFreq HW_TIMER_FREQ 1000000)
+
+add_config_library(sel4_teeos "${configure_string}")
+
+add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/src/rpmsg" rpmsg_lite)
