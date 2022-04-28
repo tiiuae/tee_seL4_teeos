7f8008e29b07b94cbf03e13884fc53da96be9cd3
Tero Salminen <tero.salminen@unikie.com>
2022-04-28 13:12:42 +0300

    Create a sub-directory for sysctllib
    Signed-off-by: Tero Salminen <tero.salminen@unikie.com>
    Change-Id: I133c0a9b301afd772e7237e1418143f2e38406c5

--- sel4_teeos.cmake
+++ sel4_teeos.cmake
@@ -14,3 +14,5 @@ config_set(HwTimerFreq HW_TIMER_FREQ 1000000)
 add_config_library(sel4_teeos "${configure_string}")
 
 add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/src/rpmsg" rpmsg_lite)
+
+add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/src/sys_ctl_service" sys_ctl_service)
